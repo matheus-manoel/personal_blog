65102c5e801da757c8f9d8317066f00de80b037e
Matheus C. Manoel <matheuscmanoel@gmail.com>
2016-01-12 20:56:18 -0200

    new, create, show and index working for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,29 @@
 class PostsController < ApplicationController
 
   def index
-    
+    @posts = Post.all.order('created_at DESC')
   end
+
+  def new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def post_params
+     params.require(:post).permit(:title, :body) 
+    end
 
 end
